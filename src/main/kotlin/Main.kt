67d45cafbb3b80344a470efc74729ec2a2ea1d84
Angela <ambengeangela@gmail.com>
2023-02-02 08:09:54 +0300

    This is My first Kotlin Assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var fullname = "Angela Adisa"
+    var age = 24
+    var phoneNumber = "0713030706"
+    var weightInKgs = 62
+    var isKenyan = true
+    var notKenyan = false
+    println(isKenyan)
+}
